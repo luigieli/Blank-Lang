8f6139217fce736cbac11269237cb19ed454d903
Marcos Marques <lmrc2@aluno.ifnmg.edu.br>
2024-07-20 13:20:52 -0300

    Add match token test
    Receive string as input and compare with expected struct as
    result

---
+++ lexer/lexer_test.go
@@ -0,0 +1,74 @@
+package lexer
+
+import (
+	"testing"
+
+	"blank/token"
+)
+
+func TestNextTokn(t *testing.T) {
+	input := `var five = 5;
+		var ten = 10;
+
+		var add = func(x, y) {
+			x + y;
+		};
+		var result = add(five, ten);`
+
+	test := []struct {
+		expectedType   token.TokenType
+		expctedLiteral string
+	}{
+		{token.VAR, "var"},
+		{token.IDENT, "five"},
+		{token.ASSIGN, "="},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+		{token.VAR, "var"},
+		{token.IDENT, "ten"},
+		{token.ASSIGN, "="},
+		{token.INT, "10"},
+		{token.SEMICOLON, ";"},
+		{token.VAR, "var"},
+		{token.IDENT, "add"},
+		{token.ASSIGN, "="},
+		{token.FUNCTION, "func"},
+		{token.LPAREN, "("},
+		{token.IDENT, "x"},
+		{token.COMMA, ","},
+		{token.IDENT, "y"},
+		{token.RPAREN, ")"},
+		{token.LBRACE, "{"},
+		{token.IDENT, "x"},
+		{token.PLUS, "+"},
+		{token.IDENT, "y"},
+		{token.SEMICOLON, ";"},
+		{token.RBRACE, "}"},
+		{token.SEMICOLON, ";"},
+		{token.VAR, "var"},
+		{token.IDENT, "result"},
+		{token.ASSIGN, "="},
+		{token.IDENT, "add"},
+		{token.LPAREN, "("},
+		{token.IDENT, "five"},
+		{token.COMMA, ","},
+		{token.IDENT, "ten"},
+		{token.RPAREN, ")"},
+		{token.SEMICOLON, ";"},
+		{token.EOF, ""},
+	}
+
+	l := New(input)
+
+	for i, tt := range test {
+		tok := l.NextToken()
+		if tok.Type != tt.expectedType {
+			t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got=%q",
+				i, tt.expectedType, tok.Type)
+		}
+		if tok.Literal != tt.expctedLiteral {
+			t.Fatalf("tests[%d] - literal wrong. expected=%q, got=%q",
+				i, tt.expctedLiteral, tok.Literal)
+		}
+	}
+}
