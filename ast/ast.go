556cdf30c6986da3ecc542dd10734486175570e3
felkng <felipersf374@gmail.com>
2024-07-21 13:11:16 -0300

    Add return statement node structure
    Co-authored-by: Luigi Eliabe  <luigi.eliabe.as@gmail.com>
    Co-authored-by: Marcos Marques <mdmarques70@gmail.com>

--- ast/ast.go
+++ ast/ast.go
@@ -44,3 +44,11 @@ func (p *Program) TokenLiteral() string {
 		return ""
 	}
 }
+
+type ReturnStatement struct {
+	Token       token.Token // the 'return' token
+	ReturnValue Expression
+}
+
+func (rs *ReturnStatement) statementNode()       {}
+func (rs *ReturnStatement) TokenLiteral() string { return rs.Token.Literal }
