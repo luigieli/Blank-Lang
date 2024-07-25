75fafbce842b7bb00ddfe860ba47ad89baed5171
Marcos Marques <mdmarques70@gmail.com>
2024-07-24 23:03:44 -0300

    Create PrefixExpression structure
    Created a strucuture to handle with prefix operators in parser
    
    Co-authored-by: Felipe Ferreira <felipersf374@gmail.com>

--- ast/ast.go
+++ ast/ast.go
@@ -109,11 +109,25 @@ type IntegerLiteral struct {
 	Value int64
 }
 
-func (il *IntegerLiteral) expressionNode()       {}
+func (il *IntegerLiteral) expressionNode()      {}
 func (il *IntegerLiteral) TokenLiteral() string { return il.Token.Literal }
 func (il *IntegerLiteral) String() string {
 	if il != nil {
 		return il.Token.Literal
+	}
+	return ""
+}
+
+type PrefixExpression struct {
+	Token token.Token
+	Right    Expression
+}
+
+func (pe *PrefixExpression) expressionNode()    {}
+func (pe *PrefixExpression) TokenLiteral() string { return pe.Token.Literal }
+func (pe *PrefixExpression) String() string {
+	if pe != nil {
+		return pe.Token.Literal
 	}
 	return ""
 }
